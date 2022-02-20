a11074f4d2df2b20ba2db11848e0801488527f98
김준오 <86396225+preparedmale@users.noreply.github.com>
2022-02-20 13:51:46 +0900

    hunger-gateway 프로젝트 생성

---
+++ src/main/java/kr/co/hunger/gateway/HungerGatewayApplication.java
@@ -0,0 +1,13 @@
+package kr.co.hunger.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HungerGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HungerGatewayApplication.class, args);
+    }
+
+}
---
+++ src/main/java/kr/co/hunger/gateway/filter/GlobalFilter.java
@@ -0,0 +1,46 @@
+package kr.co.hunger.gateway.filter;
+
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
+    public GlobalFilter() { super(Config.class); }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        // Global Pre Filter
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            log.info("Global PRE filter: request id -> {]", request.getId());
+
+            if (config.isPreLogger()) {
+                log.info("Global Filter Start: request id -> {}", request.getId());
+            }
+            // Custom Post Filter
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                if (config.isPostLogger()) {
+                    log.info("Global POST filter: response code -> {}", response.getStatusCode());
+                }
+            }));
+        };
+    }
+
+    @Data
+    public static class Config {
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
+        // Put the configuration properties
+    }
+}
